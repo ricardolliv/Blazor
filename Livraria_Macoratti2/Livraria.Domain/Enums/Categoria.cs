dbc1a5b3e8953c42eef48748b7dbe69793609fd3
Luiz Ricardo <ricardolliv@gmail.com>
2025-01-21 17:44:08 -0300

    a

---
+++ Livraria_Macoratti2/Livraria.Domain/Enums/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria.Domain.Enums
+{
+    public enum Categoria
+    {
+        Informatica = 1,
+        Ficcao,
+        Romance,
+        Humor,
+        Religiao,
+        Aventura,
+        Nenhuma
+    }
+}
---
+++ Livraria_Macoratti2/Livraria.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Livraria.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Livro>? Livros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
